c87ec30be53219d1883f3ec9a0279049c4605a1f
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 00:06:20 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -4,33 +4,14 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-CEKEXPIRED () {
-    today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | grep $MYIP | awk '{print $3}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
-    else
-    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
-    echo -e "\e[31mRenew IP letak tempoh banyak kit okay? hehe syg ktk #\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-CEKEXPIRED
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mDaftar IP dalam github lok sayang okay? mun dah daftar tapi masih juak permission denied refresh dolok website ya hehe. Love you #\e[0m"
-exit 0
-fi
+
 clear
 apt install jq curl -y
-DOMAIN=raydinz.xyz
+DOMAIN=manternet.online
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.raydinz.xyz
-CF_ID=nuridakanina@gmail.com
-CF_KEY=9e6635f2d04c860603d847383deb98fdc0777
+SUB_DOMAIN=${sub}.manternet.online
+CF_ID=anjang614@gmail.com
+CF_KEY=e943ae250caa5917be31337288655b3d9794f
 set -euo pipefail
 IP=$(wget -qO- ifconfig.me/ip);
 echo "Updating DNS for ${SUB_DOMAIN}..."
