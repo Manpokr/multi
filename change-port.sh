f9fd2655844da3b0628eaa6fe8a2170fba3b6a3b
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 21:20:17 +0800

    Update change-port.sh

--- change-port.sh
+++ change-port.sh
@@ -2,10 +2,12 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
+
+# // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-
 clear
+
 echo -e ""
 echo -e ""
 echo -e ""
@@ -15,10 +17,9 @@ echo -e "======================================"
 echo -e ""
 echo -e "     [1]  Change Port Stunnel4"
 echo -e "     [2]  Change Port OpenVPN"
-echo -e "     [3]  Change Port Wireguard"
-echo -e "     [4]  Change Port Trojan"
-echo -e "     [5]  Change Port Squid"
-echo -e "     [6]  BACK TO MENU"
+echo -e "     [3]  Change Port Trojan"
+echo -e "     [4]  Change Port Squid"
+echo -e "     [5]  BACK TO MENU"
 echo -e "======================================"
 echo -e ""
 read -p "     Select From Options [1-6 or x] :  " opt
@@ -26,10 +27,9 @@ echo -e   ""
 case $opt in
 1) clear ; port-ssl ; exit ;;
 2) clear ; port-ovpn ; exit ;;
-3) clear ; port-wg ; exit ;;
-4) clear ; port-tr ; exit ;;
-5) clear ; port-squid ; exit ;;
-6) clear ; menu ; exit ;;
+3) clear ; port-tr ; exit ;;
+4) clear ; port-squid ; exit ;;
+5) clear ; menu ; exit ;;
 x) exit ;;
-*) echo "Boh salah tekan, Sayang kedak Babi" ;;
+*) echo "Boh salah tekan " ;;
 esac
