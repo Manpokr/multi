7a4d0f4ad1829650753cb1adce501fa7e1a13fe8
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 21:41:26 +0800

    Update m-trojan.sh

--- m-trojan.sh
+++ m-trojan.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+RED='\033[0;31m'                                                                                          
+GREEN='\033[0;32m'                                                                                        
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'                                                                                         
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'                                                                                         
+NC='\033[0;37m'
+LIGHT='\033[0;37m'
+
+# // Getting 
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-
-clear 
+clear
+ 
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[0;100;33m      • TROJAN GFW MENU •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -27,5 +37,5 @@ case $opt in
 4) clear ; cek-tr ;;
 0) clear ; menu ;;
 x) exit ;;
-*) echo "Boh salah tekan, Sayang kedak Babi" ; sleep 1 ; m-trojan ;;
+*) echo "Boh salah tekan" ; sleep 1 ; m-trojan ;;
 esac
