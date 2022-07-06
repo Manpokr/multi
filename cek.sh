a0298cfa86ad1ae2016c37c611721c2c0a6bbb97
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 21:12:16 +0800

    Update cek.sh

--- cek.sh
+++ cek.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+RED='\033[0;31m'                                                                                          
+GREEN='\033[0;32m'                                                                                        
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'                                                                                         
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'                                                                                         
+NC='\033[0;37m'
+LIGHT='\033[0;37m'
+
+# // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-
-
 clear
 
 if [ -e "/var/log/auth.log" ]; then
