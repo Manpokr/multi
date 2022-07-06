c01fd41f3400d96fb7f0c8db69b04a91ba0c02b7
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 22:29:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,43 +7,29 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		echo "OpenVZ is not supported"
 		exit 1
 fi
-Blink='\e[5m'
-yell='\e[33m'
-lgreen='\e[92m'
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
 
+# // Warna
+RED='\033[0;31m'                                                                                          
+GREEN='\033[0;32m'                                                                                        
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'                                                                                         
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'                                                                                         
+NC='\033[0;37m'
+LIGHT='\033[0;37m'
+
+# // Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
 clear
+
+# // Cek Scripts
 if [ -f "/etc/rare/xray/domain" ]; then
 echo "Script Already Installed"
 exit 0
 fi
+
+# // Install
 clear
-echo ""
-echo ""
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \E[0;100;33m        • AutoScript by IANVPN •            \E[0m"
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[92m             ___   _   _  ___   _____ _  _    "
-echo -e "\e[92m            |_ _| /_\ | \| \ \ / / _ \ \| |   "
-echo -e "\e[92m             | | / _ \|    |\ V /|  _/    |   "
-echo -e "\e[92m            |___/_/ \_\_|\_| \_/ |_| |_|\_|   "
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "" 
-echo -e "$green               Telegram:$NC $red@IanVPN               $NC"
-echo -e "$green        Script Version:$NC $red VERSI LONAKK GEMOK          $NC"
-echo ''
-echo -e "$green              Proses akan mula dalam 5 Saat!            $NC"
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 5
-clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green Sila Masukkan Sub Domain (sub.yourdomain.com) $NC"
-echo -e "$green Jika tiada Sila [ Ctrl+C ] • To-Exit $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -p " Hostname / Domain: " host
 mkdir -p /etc/rare
 mkdir -p /etc/rare/xray
 mkdir -p /etc/rare/v2ray
@@ -56,88 +42,45 @@ mkdir -p /etc/systemd/system/
 mkdir -p /var/log/xray/
 mkdir -p /var/log/v2ray/
 mkdir /var/lib/premium-script;
+
 touch /etc/rare/xray/clients.txt
 touch /etc/rare/v2ray/clients.txt
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/rare/xray/domain
 echo "$host" >> /root/domain
-echo "LONAK" >> /home/version
-echo "@IanVPN" >> /home/contact
+echo "2.0 Beta" >> /home/version
+echo "Manternet" >> /home/contact
 clear
+
+# // Start
 secs_to_human() {
     echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
 }
 start=$(date +%s)
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green     Sila Tunggu sebentar             $NC"
-echo -e "$gree Process Update & Upgrade Sedang Dijalankan  $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+
+# // Update
 apt-get update && apt-get upgrade -y && update-grub -y
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green  Process Update&Upgrade Selesai        $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
-clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green Installing AutoScript IANVPN        $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
-#install ssh ovpn
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green      Install SSH OVPN               $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+
+# // Install ssh ovpn
 wget https://raw.githubusercontent.com/Manpokr/multi/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-#install ssr
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green          Install SSR                $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
-wget https://raw.githubusercontent.com/Manpokr/multi/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/Manpokr/multi/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-#install wireguard
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green        Install Wireguard              $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
-wget https://raw.githubusercontent.com/Manpokr/multi/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-#install v2ray
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green  ISSUE CERT & Install TROJAN GFW       $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+
+# // Install v2ray Trojan
 wget https://raw.githubusercontent.com/Manpokr/multi/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
-#Instal Xray
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green          Install XRAY              $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+
+# // Instal Xray
 wget https://raw.githubusercontent.com/Manpokr/multi/main/xray-go.sh && chmod +x xray-go.sh && screen -S xray-go ./xray-go.sh
-#Instal Xray
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green           Install V2RAY              $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+
+# // Instal V2ray
 wget https://raw.githubusercontent.com/Manpokr/multi/main/v2ray-go.sh && chmod +x v2ray-go.sh && screen -S v2ray-go ./v2ray-go.sh
-#set backup
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green            SET BACKUP               $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+
+# // Backup
 wget https://raw.githubusercontent.com/Manpokr/multi/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-# install OHP
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green          Install OHP                $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
+
+# Install OHP
 wget https://raw.githubusercontent.com/Manpokr/multi/main/ohp.sh && chmod +x ohp.sh && screen -S ohp ./ohp.sh
 
 rm -f /root/ssh-vpn.sh
-rm -f /root/ss.sh
-rm -f /root/ssr.sh
-rm -f /root/wg.sh
 rm -f /root/ins-vt.sh
 rm -f /root/set-br.sh
 rm -f /root/xray-go.sh
@@ -159,24 +102,15 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/Iansoftware/Script-IANVPN/main/set.sh"
+wget -O /etc/set.sh "https://raw.githubusercontent.com/Manpokr/multi/main/set.sh"
 chmod +x /etc/set.sh
 history -c
 clear
-echo " "
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green  Installation has been completed!!      $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+# // Info
 sleep 2
-echo " "
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \E[0;100;33m        • AutoScript by IANVPN •            \E[0m"
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[92m             ___   _   _  ___   _____ _  _    "
-echo -e "\e[92m            |_ _| /_\ | \| \ \ / / _ \ \| |   "
-echo -e "\e[92m             | | / _ \|    |\ V /|  _/    |   "
-echo -e "\e[92m            |___/_/ \_\_|\_| \_/ |_| |_|\_|   "
 echo ""
+echo -e "\e[33m━━━━━━━━━[\e[0m \e[32mManternet\e[0m \e[33m]━━━━━━━━━━━\e[0m"
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200"  | tee -a log-install.txt
@@ -185,10 +119,6 @@ echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8000 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81, 82"  | tee -a log-install.txt
-echo "   - Wireguard               : 7070"  | tee -a log-install.txt
-echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
-echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
-echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
 echo "   - XRAY VLESS XTLS SPLICE  : 443"  | tee -a log-install.txt
 echo "   - XRAY VLESS XTLS DIRECT  : 443"  | tee -a log-install.txt
 echo "   - XRAY VLESS WS TLS       : 443"  | tee -a log-install.txt
@@ -219,19 +149,15 @@ echo "   - Change port                     " | tee -a log-install.txt
 echo "   - Restore Data                    " | tee -a log-install.txt
 echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo ""
-echo -e "\e[33m━━━━━━━━━[\e[0m \e[32mSayang kedak Babi\e[0m \e[33m]━━━━━━━━━━━\e[0m"
+echo -e "\e[33m━━━━━━━━━[\e[0m \e[32mManternet\e[0m \e[33m]━━━━━━━━━━━\e[0m"
 echo ""
 secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
 echo -e ""
 sleep 3
 echo -e ""
+
+# // Reboot
 rm -f /root/setup.sh
 rm -f /root/.bash_history
-echo -ne "[ ${yell}WARNING${NC} ] Lepas install , tapi button y dan enter okay syg comel pendek? (y/n)? "
-read answer
-if [ "$answer" == "${answer#[Yy]}" ] ;then
-exit 0
-else
 reboot
-fi
 
