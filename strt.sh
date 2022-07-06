0507ac949d04aa6595b94be52cfbfda65f2c576f
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 22:59:26 +0800

    Update strt.sh

--- strt.sh
+++ strt.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+RED='\033[0;31m'                                                                                          
+GREEN='\033[0;32m'                                                                                                                                                                                 
+NC='\033[0;37m'
+LIGHT='\033[0;37m'
+
+# // Getting 
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 
