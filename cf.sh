d99c0777837428f408f14f46e62510acd2c5c622
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 21:16:13 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
+RED='\e[1;31m'
+GREEN='\e[0;32m'
 NC='\e[0m'
+
+# // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-
 clear
+
 apt install jq curl -y
 DOMAIN=manternet.online
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
