323c9c9729068ef4b675140b85c28807e1b3ac03
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-06 21:27:44 +0800

    Update info-menu.sh

--- info-menu.sh
+++ info-menu.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipinfo.io/ip);
+RED='\033[0;31m'                                                                                          
+GREEN='\033[0;32m'                                                                                        
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'                                                                                         
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'                                                                                         
+NC='\033[0;37m'
+LIGHT='\033[0;37m'
 
+# // Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
 clear
+
 sttt=`cat /root/log-install.txt`
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
 WKT=$(curl -s ipinfo.io/timezone )
 IPVPS=$(curl -s ipinfo.io/ip )
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \E[0;100;33m        • AutoScript by IANVPN •            \E[0m"
+echo -e " \E[0;100;33m        • AutoScript by  •            \E[0m"
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[92m             ___   _   _  ___   _____ _  _    "
-echo -e "\e[92m            |_ _| /_\ | \| \ \ / / _ \ \| |   "
-echo -e "\e[92m             | | / _ \|    |\ V /|  _/    |   "
-echo -e "\e[92m            |___/_/ \_\_|\_| \_/ |_| |_|\_|   " 
-echo -e ":
-
-   Isp Name : $ISP
-   City     : $CITY
-   Time     : $WKT
-   IPVPS    : $IPVPS
-   
-\033[1;33mScript khas untuk Si Comel Lotey Lillian Anak Alam\e[0m
-"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m
-\033[1;32mPort Information\e[0m :
-
+echo -e ""
+echo -e " Isp Name : $ISP
+echo -e " City     : $CITY
+echo -e " Time     : $WKT
+echo -e " IPVPS    : $IPVPS
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 $sttt
-
 \e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m
-
-[\033[0;32m01\e[0m] • Show VPS Informatin
-
+echo -e ""
 [00] • Back to Main Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
 echo ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
