6588fad3fb3dadfaf154cac192e72f388ef43a59
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-08 01:02:31 +0800

    Update addtrojangrpc.sh

--- add/addtrojangrpc.sh
+++ add/addtrojangrpc.sh
@@ -12,20 +12,17 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 clear
-# Getting
+
+# // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-source /var/lib/Manpokr/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat /etc/xray/domain)
-else
-domain=$IP
-fi
-domain=$(cat /etc/xray/domain)
+#domain=$(cat /etc/xray/domain)
+
+domain=$(cat /etc/rare/xray/domain)
 uuid=$(cat /proc/sys/kernel/random/uuid)
-tr="$(cat /etc/xray/trojangrpc.json | grep port | sed 's/"//g' | sed 's/port//g' | sed 's/://g' | sed 's/,//g' | sed 's/ //g')"
+xtls="$(cat ~/log-install.txt | grep -w "XRAY VLESS XTLS SPLICE" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 		read -rp "Password : " -e user
-		user_EXISTS=$(grep -w $user /etc/xray/trojangrpc.json | wc -l)
+		user_EXISTS=$(grep -w $user /etc/rare/xray/clients.txt | wc -l)
 
 		if [[ ${user_EXISTS} == '1' ]]; then
 			echo ""
@@ -33,6 +30,7 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 			exit 1
 		fi
 	done
+email=${user}@${domain}
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (Days) : " masaaktif
 read -p "SNI (bug) : " sni
@@ -40,12 +38,16 @@ read -p "Subdomain (EXP : manternet.xyz. / Press Enter If Only Using Hosts) : " 
 dom=$sub$domain
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-sed -i '/#xray-trojan-grpc$/a\#&# '"$user $exp"'\
-},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/trojangrpc.json
 
-trojanlink1="trojan://$uuid@$dom:${tr}?mode=gun&security=tls&type=grpc&serviceName=GunService&sni=${sni}#${user}"
-systemctl restart trojangrpc.service
-service cron restart
+# // Add
+echo -e "${user}\t${uuid}\t${exp}" >> /etc/rare/xray/clients.txt
+cat /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json | jq '.inbounds[0].settings.clients += [{"password": "'${uuid}'","email": "'${email}'"}]' > /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json_tmp.json
+mv -f /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json_tmp.json /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json
+
+# // Link
+trojanlink1="trojan://$uuid@$dom:${xtls}?mode=gun&security=tls&type=grpc&serviceName=xraytrojangrpc&sni=${sni}#${user}"
+systemctl restart xray.service
+service restart cron
 clear
 echo -e ""
 echo -e "================================="
@@ -56,10 +58,12 @@ echo -e "IP             : ${MYIP}"
 echo -e "Domain         : ${domain}"
 echo -e "Subdomain      : ${dom}"
 echo -e "Sni            : ${sni}"
-echo -e "port TCP       : ${tr}"
+echo -e "port TCP       : ${xtls}"
 echo -e "Password       : ${uuid}"
 echo -e "================================="
 echo -e "Link TR        : ${trojanlink1}"
 echo -e "================================="
 echo -e "Created        : $hariini"
 echo -e "Expired On     : $exp"
+echo -e "================================="
+echo -e "Script By Manternet"
