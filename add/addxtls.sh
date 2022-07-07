0bc8f026c92854cb94622761a245bc0e07a055b4
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-08 00:36:26 +0800

    Update addxtls.sh

--- add/addxtls.sh
+++ add/addxtls.sh
@@ -13,10 +13,6 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 clear
 # // Add Xtls
 #domain=$(cat /etc/xray/domain)
-#source /var/lib/Manpokr/ipvps.conf
-#sed -i '/#XRay$/a\### '"$user $exp"'\
-#},{"id": "'""$uuid""'","flow": "'xtls-rprx-direct'","email": "'""$user""'"' /etc/xray/xrayxtls.json
-
 domain=$(cat /etc/rare/xray/domain)
 xtls="$(cat ~/log-install.txt | grep -w "XRAY VLESS XTLS SPLICE" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
@@ -30,7 +26,6 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		fi
 	done
 email=${user}@${domain}
-echo -e "${user}\t${uuid}\t${exp}" >> /etc/rare/xray/clients.txt
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (days): " masaaktif
 read -p "SNI (bug) : " sni
@@ -38,14 +33,18 @@ read -p "Subdomain (EXP : manternet.xyz. / Press Enter If Only Using Hosts) : " 
 dom=$sub$domain
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 hariini=`date -d "0 days" +"%Y-%m-%d"`
+
+# // Add
+echo -e "${user}\t${uuid}\t${exp}" >> /etc/rare/xray/clients.txt
 cat /etc/rare/xray/conf/02_VLESS_TCP_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","add": "'${domain}'","flow": "xtls-rprx-direct","email": "'${email}'"}]' > /etc/rare/xray/conf/02_VLESS_TCP_inbounds_tmp.json
 mv -f /etc/rare/xray/conf/02_VLESS_TCP_inbounds_tmp.json /etc/rare/xray/conf/02_VLESS_TCP_inbounds.json
 
+# // Link Xtls
 IP=$( curl -s ipinfo.io/ip )
-vd="vless://$uuid@$dom:$port?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-direct&sni=$sni#$user"
-vu="vless://$uuid@$dom:$port?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-direct-udp443&sni=$sni#$user"
-vs="vless://$uuid@$dom:$port?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-splice&sni=$sni#$user"
-vsu="vless://$uuid@$dom:$port?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-splice-udp443&sni=$sni#$user"
+vd="vless://$uuid@$dom:$xtls?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-direct&sni=$sni#$user"
+vu="vless://$uuid@$dom:$xtls?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-direct-udp443&sni=$sni#$user"
+vs="vless://$uuid@$dom:$xtls?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-splice&sni=$sni#$user"
+vsu="vless://$uuid@$dom:$xtls?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-splice-udp443&sni=$sni#$user"
 systemctl restart xray.service
 clear
 echo -e "================================="
@@ -56,7 +55,7 @@ echo -e "IP/Host        : ${IP}"
 echo -e "Domain         : ${domain}"
 echo -e "Subdomain      : ${dom}"
 echo -e "Sni            : ${sni}"
-echo -e "port TCP-XTLS  : $port"
+echo -e "port TCP-XTLS  : $xtls"
 echo -e "id             : ${uuid}"
 echo -e "================================="
 echo -e "Direct         : ${vd}"
