8c271f003c82d95d94ba12fbfe109a65dd19d419
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-08 23:08:38 +0800

    Update change-port.sh

--- change-port.sh
+++ change-port.sh
@@ -9,17 +9,18 @@ echo "Checking VPS"
 clear
 
 echo -e ""
-echo -e ""
-echo -e ""
 echo -e "======================================"
 echo -e "       Change Port All Service"
 echo -e "======================================"
 echo -e ""
-echo -e "     [1]  Change Port Stunnel4"
-echo -e "     [2]  Change Port OpenVPN"
-echo -e "     [3]  Change Port Trojan"
-echo -e "     [4]  Change Port Squid"
-echo -e "     [5]  BACK TO MENU"
+echo -e "     [•1]  Change Port Stunnel4"
+echo -e "     [•2]  Change Port OpenVPN"
+echo -e "     [•3]  Change Port Trojan"
+echo -e "     [•4]  Change Port Squid"
+echo -e "     [•5]  Change Port Xray"
+echo -e "     [•6]  Change Port V2ray"
+echo -e ""
+echo -e "     [•x]  Back To Menu"
 echo -e "======================================"
 echo -e ""
 read -p "     Select From Options [1-6 or x] :  " opt
@@ -29,7 +30,8 @@ case $opt in
 2) clear ; port-ovpn ; exit ;;
 3) clear ; port-tr ; exit ;;
 4) clear ; port-squid ; exit ;;
-5) clear ; menu ; exit ;;
-x) exit ;;
+5) clear ; port-xray ; exit ;;
+6) clear ; port-v2ray ; exit ;;
+x) clear ; menu ; exit ;;
 *) echo "Boh salah tekan " ;;
 esac
