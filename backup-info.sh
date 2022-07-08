da8a0fbec1d08e2671dcf4bbf592e087ad7e0740
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-08 22:42:57 +0800

    Update backup-info.sh

--- backup-info.sh
+++ backup-info.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+LIGHT='\033[0;37m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
@@ -11,29 +12,29 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 cd
 NameUser=$(curl -sS https://raw.githubusercontent.com/Manpokr/mon/main/ip | grep $MYIP | awk '{print $2}')
-cekdata=$(curl -sS https://raw.githubusercontent.com/Iansoftware/user-backupv1/main/$NameUser/$NameUser-last-backup | grep 404 | awk '{print $1}' | cut -d: -f1)
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m        • BACKUP HISTORY •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+cekdata=$(curl -sS https://raw.githubusercontent.com/Manpokr/user-backupv1/main/$NameUser/$NameUser-last-backup | grep 404 | awk '{print $1}' | cut -d: -f1)
+echo -e "=================================="
+echo -e "            BACKUP INFO           "
+echo -e "=================================="
 echo -e ""
 [[ "$cekdata" = "404" ]] && {
 red "Data not found / you never backup"
 echo
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "=================================="
 echo
 read -n 1 -s -r -p "Press any key to back on menu"
 m-system
 } || {
 green "Data found for username $NameUser"
 } 
-data=$(curl -sS https://raw.githubusercontent.com/Iansoftware/user-backupv1/main/$NameUser/$NameUser-last-backup)
+data=$(curl -sS https://raw.githubusercontent.com/Manpokr/user-backupv1/main/$NameUser/$NameUser-last-backup)
 echo
 echo -e "[ ${green}INFO${NC} ] • Getting info database backup history..."
 sleep 2
 echo
 echo -e "$data"
 echo
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "=================================="
 echo
 read -n 1 -s -r -p "Press any key to back on menu"
 m-system
