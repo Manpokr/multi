934d29059dd62774b3e5fc31d94757519559f797
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-09 00:00:20 +0800

    Update and rename cektrojan.sh to cekxray.sh

---
+++ cek/cekxray.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Trojan Cek
+# ======================
+
+# Color
+RED="\e[1;31m"
+GREEN="\e[0;32m"
+NC="\e[0m"
+
+###################
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###################
+
+# Validate Your IP Address
+clear
+MYIP=$(wget -qO- ipinfo.io/ip);
+data=($(cat /etc/rare/xray/clients.txt | awk '{print $1}'));
+echo "-------------------------------";
+echo "-----=[ Trojan User Login ]=-----";
+echo "-------------------------------";
+for akun in "${data[@]}"
+do
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
+echo -n > /tmp/ipvmess.txt
+data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
+for ip in "${data2[@]}"
+do
+jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+if [[ "$jum" = "$ip" ]]; then
+echo "$jum" >> /tmp/ipvmess.txt
+else
+echo "$ip" >> /tmp/other.txt
+fi
+jum2=$(cat /tmp/ipvmess.txt)
+sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
+done
+jum=$(cat /tmp/ipvmess.txt)
+if [[ -z "$jum" ]]; then
+echo > /dev/null
+else
+jum2=$(cat /tmp/ipvmess.txt | nl)
+echo "user : $akun";
+echo "$jum2";
+echo "-------------------------------"
+done
+read -p "Press Enter For Back To Xray Menu/ CTRL+C To Cancel : "
+xray-menu
