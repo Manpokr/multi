1c966dea022eb733f6f9286e921d1780fbe200f6
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-11 04:22:00 +0800

    Update xray-menu.sh

--- xray-menu.sh
+++ xray-menu.sh
@@ -19,7 +19,7 @@ function add-user() {
     echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
 	read -p "Username  : " user
-	if grep -qw "$user" /etc/rare/xray/clients.txt; then
+	if grep -qw "$user" /etc/xray/clients.txt; then
 		echo -e ""
 		echo -e "User \e[31m$user\e[0m already exist"
 		echo -e ""
@@ -33,13 +33,13 @@ function add-user() {
 	uuid=$(cat /proc/sys/kernel/random/uuid)
 	exp=$(date -d +${duration}days +%Y-%m-%d)
 	expired=$(date -d "${exp}" +"%d %b %Y")
-	domain=$(cat /etc/rare/xray/domain)
+	domain=$(cat /etc/xray/domain)
 	xtls="$(cat ~/log-install.txt | grep -w "XRAY VLESS XTLS SPLICE" | cut -d: -f2|sed 's/ //g')"
 	email=${user}@${domain}
     cat>/etc/rare/xray/tls.json<<EOF
       {
        "v": "2",
-       "ps": "${user}@IanVPN",
+       "ps": "${user}",
        "add": "${BUG}.${domain}",
        "port": "${xtls}",
        "id": "${uuid}",
@@ -54,171 +54,41 @@ function add-user() {
 }
 EOF
     vmess_base641=$( base64 -w 0 <<< $vmess_json1)
-    vmesslink1="vmess://$(base64 -w 0 /etc/rare/xray/tls.json)"
-	echo -e "${user}\t${uuid}\t${exp}" >> /etc/rare/xray/clients.txt
-    cat /etc/rare/xray/conf/02_VLESS_TCP_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","add": "'${domain}'","flow": "xtls-rprx-direct","email": "'${email}'"}]' > /etc/rare/xray/conf/02_VLESS_TCP_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/02_VLESS_TCP_inbounds_tmp.json /etc/rare/xray/conf/02_VLESS_TCP_inbounds.json
+    vmesslink1="vmess://$(base64 -w 0 /etc/xray/tls.json)"
+	echo -e "${user}\t${exp}" >> /etc/xray/clients.txt
+    cat /etc/mon/xray/conf/02_trojan_TCP_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","add": "'${domain}'","flow": "xtls-rprx-direct","email": "'${email}'"}]' > /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json /etc/mon/xray/conf/02_trojan_TCP_inbounds.json
 
-    cat /etc/rare/xray/conf/03_VLESS_WS_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","email": "'${email}'"}]' > /etc/rare/xray/conf/03_VLESS_WS_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/03_VLESS_WS_inbounds_tmp.json /etc/rare/xray/conf/03_VLESS_WS_inbounds.json
+    cat /etc/mon/xray/conf/03_VLESS_WS_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","email": "'${email}'"}]' > /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json /etc/mon/xray/conf/03_VLESS_WS_inbounds.json
 
-    cat /etc/rare/xray/conf/04_trojan_TCP_inbounds.json | jq '.inbounds[0].settings.clients += [{"password": "'${uuid}'","email": "'${email}'"}]' > /etc/rare/xray/conf/04_trojan_TCP_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/04_trojan_TCP_inbounds_tmp.json /etc/rare/xray/conf/04_trojan_TCP_inbounds.json
+    cat /etc/mon/xray/conf/04_trojan_TCP_inbounds.json | jq '.inbounds[0].settings.clients += [{"password": "'${uuid}'","email": "'${email}'"}]' > /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json /etc/mon/xray/conf/04_trojan_TCP_inbounds.json
 
-    cat /etc/rare/xray/conf/05_VMess_WS_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","alterId": 0,"add": "'${domain}'","email": "'${email}'"}]' > /etc/rare/xray/conf/05_VMess_WS_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/05_VMess_WS_inbounds_tmp.json /etc/rare/xray/conf/05_VMess_WS_inbounds.json
+    cat /etc/mon/xray/conf/05_VMess_WS_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","alterId": 0,"add": "'${domain}'","email": "'${email}'"}]' > /etc/mon/xray/conf/05_VMess_WS_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/05_VMess_WS_inbounds_tmp.json /etc/mon/xray/conf/05_VMess_WS_inbounds.json
 
-    cat /etc/rare/xray/conf/06_VLESS_gRPC_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","alterId": 0,"add": "'${domain}'","email": "'${email}'"}]' > /etc/rare/xray/conf/06_VLESS_gRPC_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/06_VLESS_gRPC_inbounds_tmp.json /etc/rare/xray/conf/06_VLESS_gRPC_inbounds.json
+    cat /etc/mon/xray/conf/06_VLESS_gRPC_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","alterId": 0,"add": "'${domain}'","email": "'${email}'"}]' > /etc/mon/xray/conf/06_VLESS_gRPC_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/06_VLESS_gRPC_inbounds_tmp.json /etc/mon/xray/conf/06_VLESS_gRPC_inbounds.json
 
-    cat /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json | jq '.inbounds[0].settings.clients += [{"password": "'${uuid}'","email": "'${email}'"}]' > /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json_tmp.json
-	mv -f /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json_tmp.json /etc/rare/xray/conf/04_trojan_gRPC_inbounds.json
+    cat /etc/mon/xray/conf/04_trojan_gRPC_inbounds.json | jq '.inbounds[0].settings.clients += [{"password": "'${uuid}'","email": "'${email}'"}]' > /etc/mon/xray/conf/04_trojan_gRPC_inbounds.json_tmp.json
+	mv -f /etc/mon/xray/conf/04_trojan_gRPC_inbounds.json_tmp.json /etc/mon/xray/conf/04_trojan_gRPC_inbounds.json
 
-    cat <<EOF >>"/etc/rare/config-user/${user}"
+    cat <<EOF >>"/etc/mon/config-user/${user}"
 
-vless://$uuid@$domain:$xtls?flow=xtls-rprx-direct&encryption=none&security=xtls&sni=$BUG&type=tcp&headerType=none&host=$BUG#$user
-vless://$uuid@$domain:$xtls?flow=xtls-rprx-splice&encryption=none&security=xtls&sni=$BUG&type=tcp&headerType=none&host=$BUG#$user
-vless://$uuid@$domain:$xtls?encryption=none&security=xtls&sni=$BUG&type=ws&host=$BUG&path=xrayws#$user
-trojan://$uuid@$domain:$xtls?sni=$BUG#$user@IanVPN
-trojan://$uuid@$BUG.domain:${xtls}?mode=gun&security=tls&type=grpc&serviceName=xraytrojangrpc&sni=${BUG}#$user
-vless://${uuid}@$BUG.$domain:${xtls}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=xraygrpc&sni=$BUG#$user
+vless://$uuid@$domain:$xtls?security=tls&encryption=none&type=ws&headerType=none&path=/xrayws&sni=$BUG#$user
+vless://$uuid@$domain:$xtls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=xraygrpc&sni=$BUG#$user
+vless://$uuid@$domain:$xtls?security=tls&encryption=none&type=tcp&${BUG}#${user}
+
+trojan://$uuid@$domain:$xtls?sni=$BUG#$user
+trojan://$uuid@$domain:$xtls?mode=gun&security=tls&type=grpc&serviceName=xraytrojangrpc&sni=${BUG}#$user
+trojan://$uuid@$domain:$xtls?flow=xtls-rprx-direct&encryption=none&security=xtls&sni=$BUG&type=tcp&headerType=none&host=$BUG#$user
+
 ${vmesslink1}
-EOF
-    cat <<EOF >>"/etc/rare/config-url/${user}"
-# =======================================
-# XRAY CORE CONFIG MERLIN CLASH ASUS
-# By IanVPN telegram: @IanVPN
-# =======================================
-proxies:
-  - {name: VLess Splice ${user}@IanVPN, server: $BUG.$domain, port: $xtls, type: vless, flow: xtls-rprx-splice, uuid: $uuid, cipher: auto, tls: true, skip-cert-verify: true, network: tcp, sni: $BUG, udp: true}
-  - {name: VLess Direct ${user}@IanVPN, server: $BUG.$domain, port: $xtls, type: vless, flow: xtls-rprx-direct, uuid: $uuid, cipher: auto, tls: true, skip-cert-verify: true, network: tcp, sni: $BUG, udp: true}
-  - {name: VLess WS ${user}@IanVPN, server: $BUG.$domain, port: $xtls, type: vless, flow: xtls-rprx-direct, uuid: $uuid, cipher: auto, tls: true, skip-cert-verify: true, network: ws, ws-path: /xrayws, ws-headers: {Host: $BUG}, sni: $BUG, udp: true}
-  - {name: Trojan ${user}@IanVPN, server: $domain, port: $xtls, type: trojan, password: $uuid, sni: $BUG, skip-cert-verify: true, udp: true}
-  - {name: VMess ${user}@IanVPN, server: $domain, port: $xtls, type: vmess, uuid: $uuid, alterId: 0, cipher: auto, tls: true, skip-cert-verify: true, network: ws, ws-path: /xrayvws, ws-headers: {Host: $BUG}, udp: true}
-  - {name: VMess SNI ${user}@IanVPN, server: $domain, port: $xtls, type: vmess, uuid: $uuid, alterId: 0, cipher: auto, tls: true, skip-cert-verify: true, network: ws, ws-path: /xrayvws, ws-headers: {Host: $BUG}, sni: $BUG, udp: true}
-port: 3333
-socks-port: 23456
-redir-port: 23457
-allow-lan: true
-mode: global
-log-level: error
-external-controller: 192.168.50.1:9990
-experimental:
-  ignore-resolve-fail: true
-external-ui: dashboard
-secret: "clash"
-profile:
-  store-selected: true
-ipv6: false
 
-hosts:
-  router.asus.com: 192.168.50.1
-  services.googleapis.cn: 74.125.193.94
-
-dns:
-  enable: true
-  ipv6: false
-  listen: :23453
-  enhanced-mode: fake-ip
-  fake-ip-range: 198.18.0.1/16 
-  fake-ip-filter:
-    - '*.lan'
-    - '*.linksys.com'
-    - '*.linksyssmartwifi.com'
-    - 'swscan.apple.com'
-    - 'mesu.apple.com'
-    - '*.msftconnecttest.com'
-    - '*.msftncsi.com'
-    # === NTP Service ===
-    - 'time.*.com'
-    - 'time.*.gov'
-    - 'time.*.edu.cn'
-    - 'time.*.apple.com'
-    - 'time1.*.com'
-    - 'time2.*.com'
-    - 'time3.*.com'
-    - 'time4.*.com'
-    - 'time5.*.com'
-    - 'time6.*.com'
-    - 'time7.*.com'
-    - 'ntp.*.com'
-    - 'ntp.*.com'
-    - 'ntp1.*.com'
-    - 'ntp2.*.com'
-    - 'ntp3.*.com'
-    - 'ntp4.*.com'
-    - 'ntp5.*.com'
-    - 'ntp6.*.com'
-    - 'ntp7.*.com'
-    - '*.time.edu.cn'
-    - '*.ntp.org.cn'
-    - '+.pool.ntp.org'
-    - 'time1.cloud.tencent.com'
-    # === Music Service ===
-    ## NetEase
-    - '+.music.163.com'
-    - '+.126.net'
-    ## Baidu
-    - 'musicapi.taihe.com'
-    - 'music.taihe.com'
-    ## Kugou
-    - 'songsearch.kugou.com'
-    - 'trackercdn.kugou.com'
-    ## Kuwo
-    - '*.kuwo.cn'
-    ## JOOX
-    - 'api-jooxtt.sanook.com'
-    - 'api.joox.com'
-    - 'joox.com'
-    ## QQ
-    - '+.music.tc.qq.com'
-    - 'aqqmusic.tc.qq.com'
-    - '+.stream.qqmusic.qq.com'
-    ## Xiami
-    - '+.xiami.com'
-    ## Migu
-    - '+.music.migu.cn'
-    # === Game Service ===
-    ## Nintendo Switch
-    - '+.srv.nintendo.net'
-    ## Sony PlayStation
-    - '+.stun.playstation.net'
-    ## Microsoft Xbox
-    - 'xbox.*.microsoft.com'
-    - '+.xboxlive.com'
-    # === Other ===
-    ## QQ Quick Login
-    - 'localhost.ptlogin2.qq.com'
-    ## Golang
-    - 'proxy.golang.org'
-    ## STUN Server
-    - 'stun.*.*'
-    - 'stun.*.*.*'
-    # === ?? ===
-    - '+.qq.com'
-    - '+.baidu.com'
-    - '+.163.com'
-    - '+.126.net'
-    - '+.taobao.com'
-    - '+.jd.com'
-    - '+.tmall.com'
-  nameserver:
-    - 119.29.29.29
-    - 223.5.5.5
-    - 180.76.76.76
-  fallback:
-    - https://doh.dns.sb/dns-query
-    - tcp://208.67.222.222:443
-    - tls://dns.google
-  fallback-filter:
-    geoip: true
-    ipcidr:
-      - 240.0.0.0/4
-tproxy: true
-tproxy-port: 23458
 EOF
-	base64Result=$(base64 -w 0 /etc/rare/config-user/${user})
-    echo ${base64Result} >"/etc/rare/config-url/${uuid}"
+
+	
     systemctl restart xray.service
     echo -e "\033[32m[Info]\033[0m xray Start Successfully !"
     sleep 2
@@ -233,16 +103,18 @@ echo -e "Subdomain      : ${dom}"
 echo -e "Sni/Bug        : ${BUG}"
 echo -e "Port           : $xtls"
 echo -e "id             : ${uuid}"
+echo -e "=================================""
+echo -e "Vless Grpc     : vless://$uuid@$domain:$xtls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=xraygrpc&sni=$BUG#$user"
 echo -e "================================="
-echo -e "Vless Xtls     : vless://$uuid@$BUG.$domain:$xtls?flow=xtls-rprx-splice&encryption=none&security=tls&sni=$BUG&type=tcp&headerType=none&host=$BUG#$user"
+echo -e "Vless Ws       : vless://$uuid@$domain:$xtls?encryption=none&security=tls&sni=$BUG&type=ws&host=$BUG&path=/xrayws#$user"
 echo -e "================================="
-echo -e "Vless Ws       : vless://$uuid@$BUG.$domain:$xtls?encryption=none&security=tls&sni=$BUG&type=ws&host=$BUG&path=/xrayws#$user"
+echo -e "Vless Tcp Tls  : vless://$uuid@$domain:$xtls?security=tls&encryption=none&type=tcp&${BUG}#${user}"
 echo -e "================================="
-echo -e "Trojan         : trojan://$uuid@$BUG.$domain:$xtls?sni=$BUG#$user"
+echo -e "Trojan Tcp     : trojan://$uuid@$domain:$xtls?sni=$BUG#$user"
 echo -e "================================="
-echo -e "Trojan Grpc    : trojan://$uuid@$BUG.domain:${xtls}?mode=gun&security=tls&type=grpc&serviceName=xraytrojangrpc&sni=${BUG}#$user "
+echo -e "Trojan Grpc    : trojan://$uuid@$domain:${xtls}?mode=gun&security=tls&type=grpc&serviceName=xraytrojangrpc&sni=${BUG}#$user "
 echo -e "================================="
-echo -e "Vless Grpc     : vless://${uuid}@$BUG.$domain:${xtls}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=xraygrpc&sni=$BUG#$user"
+echo -e "Trojan Xtls    : trojan://$uuid@$domain:$xtls?flow=xtls-rprx-direct&encryption=none&security=xtls&sni=$BUG&type=tcp&headerType=none&host=$BUG#$user"
 echo -e "================================="
 echo -e "Vmess          : ${vmesslink1}"
 echo -e "================================="
@@ -260,7 +132,7 @@ function delete-user() {
     echo -e ""  
 	read -p "Username : " user
 	echo -e ""
-	if ! grep -qw "$user" /etc/rare/xray/clients.txt; then
+	if ! grep -qw "$user" /etc/xray/clients.txt; then
 		echo -e ""
         echo -e "User \e[31m$user\e[0m does not exist"
         echo ""
@@ -269,19 +141,19 @@ function delete-user() {
         read -n 1 -s -r -p "Press any key to back on menu"
         xray-menu   
 	fi
-    rm /etc/rare/config-url/${user}
-	uuid="$(cat /etc/rare/xray/clients.txt | grep -w "$user" | awk '{print $2}')"
-	cat /etc/rare/xray/conf/02_VLESS_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/rare/xray/conf/02_VLESS_TCP_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/02_VLESS_TCP_inbounds_tmp.json /etc/rare/xray/conf/02_VLESS_TCP_inbounds.json
-    cat /etc/rare/xray/conf/03_VLESS_WS_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/rare/xray/conf/03_VLESS_WS_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/03_VLESS_WS_inbounds_tmp.json /etc/rare/xray/conf/03_VLESS_WS_inbounds.json
-    cat /etc/rare/xray/conf/04_trojan_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.password == "'${uuid}'"))' > /etc/rare/xray/conf/04_trojan_TCP_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/04_trojan_TCP_inbounds_tmp.json /etc/rare/xray/conf/04_trojan_TCP_inbounds.json		
-    cat /etc/rare/xray/conf/05_VMess_WS_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/rare/xray/conf/05_VMess_WS_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/05_VMess_WS_inbounds_tmp.json /etc/rare/xray/conf/05_VMess_WS_inbounds.json
-    sed -i "/\b$user\b/d" /etc/rare/xray/clients.txt
-    rm /etc/rare/config-user/${user}
-    rm /etc/rare/config-url/${uuid}
+    rm /etc/config-url/${user}
+	uuid="$(cat /etc/xray/clients.txt | grep -w "$user" | awk '{print $2}')"
+	cat /etc/mon/xray/conf/02_trojan_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json /etc/mon/xray/conf/02_trojan_TCP_inbounds.json
+    cat /etc/mon/xray/conf/03_VLESS_WS_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json /etc/mon/xray/conf/03_VLESS_WS_inbounds.json
+    cat /etc/mon/xray/conf/04_trojan_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.password == "'${uuid}'"))' > /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json /etc/mon/xray/conf/04_trojan_TCP_inbounds.json		
+    cat /etc/mon/xray/conf/05_VMess_WS_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/05_VMess_WS_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/05_VMess_WS_inbounds_tmp.json /etc/mon/xray/conf/05_VMess_WS_inbounds.json
+    sed -i "/\b$user\b/d" /etc/mon/xray/clients.txt
+    rm /etc/config-user/${user}
+    rm /etc/config-url/${uuid}
 	systemctl restart xray.service
     echo -e "\033[32m[Info]\033[0m xray Start Successfully !"
     echo ""
@@ -300,7 +172,7 @@ function extend-user() {
     echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
 	read -p "Username : " user
-	if ! grep -qw "$user" /etc/rare/xray/clients.txt; then
+	if ! grep -qw "$user" /etc/xray/clients.txt; then
 		echo -e ""
 		echo -e "User \e[31m$user\e[0m does not exist"
         echo ""
@@ -310,14 +182,14 @@ function extend-user() {
         xray-menu 
 	fi
 	read -p "Duration (day) : " extend
-	uuid=$(cat /etc/rare/xray/clients.txt | grep -w $user | awk '{print $2}')
-	exp_old=$(cat /etc/rare/xray/clients.txt | grep -w $user | awk '{print $3}')
+	uuid=$(cat /etc/xray/clients.txt | grep -w $user | awk '{print $2}')
+	exp_old=$(cat /etc/xray/clients.txt | grep -w $user | awk '{print $3}')
 	diff=$((($(date -d "${exp_old}" +%s)-$(date +%s))/(86400)))
 	duration=$(expr $diff + $extend + 1)
 	exp_new=$(date -d +${duration}days +%Y-%m-%d)
 	exp=$(date -d "${exp_new}" +"%d %b %Y")
-	sed -i "/\b$user\b/d" /etc/rare/xray/clients.txt
-	echo -e "$user\t$uuid\t$exp_new" >> /etc/rare/xray/clients.txt
+	sed -i "/\b$user\b/d" /etc/xray/clients.txt
+	echo -e "$user\t$uuid\t$exp_new" >> /etc/xray/clients.txt
 	clear
 	echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e "\E[0;100;33m    • XRAY USER INFORMATION •      \E[0m"
@@ -346,8 +218,8 @@ function user-list() {
 		exp=$(echo $expired | awk '{print $3}')
 		exp_date=$(date -d"${exp}" "+%d %b %Y")
 		printf "%-17s %2s\n" "$user" "     $exp_date"
-	done < /etc/rare/xray/clients.txt
-	total=$(wc -l /etc/rare/xray/clients.txt | awk '{print $1}')
+	done < /etc/xray/clients.txt
+	total=$(wc -l /etc/xray/clients.txt | awk '{print $1}')
     echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e "Total accounts: $total"
     echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -359,7 +231,7 @@ function user-list() {
 function user-monitor() {
     clear
 	echo -n > /tmp/other.txt
-	data=($(cat /etc/rare/xray/clients.txt | awk '{print $1}'));
+	data=($(cat /etc/xray/clients.txt | awk '{print $1}'));
 	echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e "\E[0;100;33m      • XRAY USER MONITOR •        \E[0m"
     echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -409,7 +281,7 @@ function show-config() {
     echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo -e ""
 	read -p "User : " user
-	if ! grep -qw "$user" /etc/rare/xray/clients.txt; then
+	if ! grep -qw "$user" /etc/xray/clients.txt; then
 		echo -e ""
 		echo -e "User \e[31m$user\e[0m does not exist"
 		echo -e ""
@@ -417,10 +289,10 @@ function show-config() {
         xray-menu
 	fi
     xtls="$(cat ~/log-install.txt | grep -w "XRAY VLESS XTLS SPLICE" | cut -d: -f2|sed 's/ //g')"
-    link=$(cat /etc/rare/config-user/${user})
-	uuid=$(cat /etc/rare/xray/clients.txt | grep -w "$user" | awk '{print $2}')
-	domain=$(cat /etc/rare/xray/domain)
-	exp=$(cat /etc/rare/xray/clients.txt | grep -w "$user" | awk '{print $3}')
+    link=$(cat /etc/config-user/${user})
+	uuid=$(cat /etc/xray/clients.txt | grep -w "$user" | awk '{print $2}')
+	domain=$(cat /etc/xray/domain)
+	exp=$(cat /etc/rare/clients.txt | grep -w "$user" | awk '{print $3}')
 	exp_date=$(date -d"${exp}" "+%d %b %Y")
 
 	clear
