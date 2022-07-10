18f51339223a50b4e51336ec5a44c996bd59767c
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-11 04:43:48 +0800

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -5,7 +5,7 @@ NC='\033[0;37m'
 LIGHT='\033[0;37m'
 
 # // Getting
-domain=$(cat /root/domain)
+domain=$(cat /etc/xray/domain)
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
@@ -24,25 +24,6 @@ mkdir /etc/xray/tls/
 mkdir -p /etc/trojan/
 touch /etc/trojan/akun.conf
 
-# // Install Cert
-bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
-mkdir /root/.acme.sh
-curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
-chmod +x /root/.acme.sh/acme.sh
-sudo pkill -f nginx & wait $!
-systemctl stop nginx
-sleep 2
-
-# // Acme
-/root/.acme.sh/acme.sh  --upgrade  --auto-upgrade
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --server letsencrypt >> /etc/tls/$domain.log
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
-cat /etc/tls/$domain.log
-systemctl daemon-reload
-systemctl restart nginx
-service squid start
-H
 # // Cp Json
 uuid=$(cat /proc/sys/kernel/random/uuid)
 cat <<EOF > /etc/trojan/config.json
