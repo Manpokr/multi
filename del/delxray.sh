23c2e4c45fb56d8774ae8c9c477b48c2690b1742
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-15 02:45:11 +0800

    Update delxray.sh

--- del/delxray.sh
+++ del/delxray.sh
@@ -13,27 +13,21 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/clients.txt")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo ""
-		echo "You have no existing clients!"
-		exit 1
+	echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[0;100;33m       • DELETE XRAY USER •        \E[0m"
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e ""  
+	read -p "Username : " user
+	echo -e ""
+	if ! grep -qw "$user" /etc/rare/xray/clients.txt; then
+		echo -e ""
+        echo -e "User \e[31m$user\e[0m does not exist"
+        echo ""
+        echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo ""
+        read -n 1 -s -r -p "Press any key to back on menu"
+        xray-menu   
 	fi
-
-	echo ""
-	echo " Client Xray Remove"
-	echo " Press CTRL+C to return"
-	echo " ==============================="
-	echo "     No  User  Expired "
-	grep -E "^#&# " "/etc/xray/clients.txt" | cut -d ' ' -f 2-3 | nl -s ') '
-	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
-		fi
-	done
-
 uuid="$(cat /etc/xray/clients.txt | grep -w "$user" | awk '{print $2}')"
 
 	cat /etc/mon/xray/conf/02_trojan_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json
@@ -48,13 +42,14 @@ uuid="$(cat /etc/xray/clients.txt | grep -w "$user" | awk '{print $2}')"
 
 
 
-
-user=$(grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojan.json
-sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojan.json
+user=$(echo $expired | awk '{print $1}')
+exp=$(echo $expired | awk '{print $3}')
+#user=$(grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+#exp=$(grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+#sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojan.json
+#sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojan.json
 systemctl restart xray.service
-service cron restart
+#service cron restart
 clear
 echo ""
 echo "================================"
