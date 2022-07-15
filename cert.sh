426a0dc958e61fabd6491c7ea20af23ec84a805f
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-15 18:41:41 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -6,6 +6,9 @@ RED='\e[1;31m'
 GREEN='\e[0;32m'
 NC='\e[0m'
 
+# // Domain
+domain=$(cat /root/domain)
+domain=$(cat /etc/xray/domain)
 # // Install Cert
 mkdir /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
@@ -17,6 +20,7 @@ sleep 2
 # // Acme
 /root/.acme.sh/acme.sh  --upgrade  --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --register-account -m anjang614@gmail.com
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --server letsencrypt >> /etc/tls/$domain.log
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 cat /etc/tls/$domain.log
@@ -24,6 +28,7 @@ systemctl daemon-reload
 systemctl restart nginx
 service squid start
 
+restart
 echo -e "done"
 sleep 2
 clear
