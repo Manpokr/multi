930053e0df3cd156d4f5bb0152ff717d99bf7d20
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-15 16:57:40 +0800

    Update xray-xp.sh

--- xray-xp.sh
+++ xray-xp.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
+RED='\033[0;31m'                                                                                          
+GREEN='\033[0;32m'                                                                                                                                                                                 
+NC='\033[0;37m'
+
+# // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 
+
 clear
 
+# // Autodel
 today=$(date -d +1day +%Y-%m-%d)
-
 while read expired
 do
 	user=$(echo $expired | awk '{print $1}')
-	uuid=$(echo $expired | awk '{print $2}')
-	exp=$(echo $expired | awk '{print $3}')
+	uuid=$(echo $expired | awk '{print $3}')
+	exp=$(echo $expired | awk '{print $2}')
 
 	if [[ $exp < $today ]]; then
 		rm /etc/config-url/${user}
-		cat /etc/mon/xray/conf/02_trojan_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json
-		mv -f /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json /etc/mon/xray/conf/02_trojan_TCP_inbounds_tmp.json
+		cat /etc/mon/xray/conf/02_VLESS_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/02_VLESS_TCP_inbounds_tmp.json
+		mv -f /etc/mon/xray/conf/02_VLESS_TCP_inbounds_tmp.json /etc/mon/xray/conf/02_VLESS_TCP_inbounds_tmp.json
 		cat /etc/mon/xray/conf/03_VLESS_WS_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json
 		mv -f /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json /etc/mon/xray/conf/03_VLESS_WS_inbounds.json
 		cat /etc/mon/xray/conf/04_trojan_TCP_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.password == "'${uuid}'"))' > /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json
 		mv -f /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json /etc/mon/xray/conf/04_trojan_TCP_inbounds.json
 		cat /etc/mon/xray/conf/05_VMess_WS_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/05_VMess_WS_inbounds_tmp.json
 		mv -f /etc/mon/xray/conf/05_VMess_WS_inbounds_tmp.json /etc/mon/xray/conf/05_VMess_WS_inbounds.json
-		sed -i "/\b$user\b/d" /etc/xray/clients.txt
+
+                # // Grpc		
+                cat /etc/mon/xray/conf/04_trojan_gRPC_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.password == "'${uuid}'"))' > /etc/mon/xray/conf/04_trojan_gRPC_inbounds_tmp.json
+		mv -f /etc/mon/xray/conf/04_trojan_gRPC_inbounds_tmp.json /etc/mon/xray/conf/04_trojan_gRPC_inbounds.json
+                cat /etc/mon/xray/conf/06_VLESS_gRPC_inbounds.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /etc/mon/xray/conf/06_VLESS_gRPC_inbounds_tmp.json
+		mv -f /etc/mon/xray/conf/06_VLESS_gRPC_inbounds_tmp.json /etc/mon/xray/conf/06_VLESS_gRPC_inbounds.json
+
+                sed -i "/\b$user\b/d" /etc/xray/clients.txt
 		rm /etc/config-user/${user}
 		rm /etc/config-url/${uuid}
 	fi
