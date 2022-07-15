7e677907a1cb5cf5295cdbd4da74c6f393ee1bcc
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-15 23:11:24 +0800

    Update m-webmin.sh

--- m-webmin.sh
+++ m-webmin.sh
@@ -20,11 +20,7 @@ cek=$(netstat -ntlp | grep 10000 | awk '{print $7}' | cut -d'/' -f2)
 function install () {
 IP=$(wget -qO- ifconfig.me/ip);
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m        • INSTALL WEBMIN •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 0.5
-echo ""
+
 echo -e "\033[32m[Info]\033[0m Adding Repositori Webmin"
 sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
 apt install gnupg gnupg1 gnupg2 -y > /dev/null 2>&1
@@ -42,18 +38,23 @@ rm -f /root/jcameron-key.asc > /dev/null 2>&1
 sleep 0.5
 echo -e "\033[32m[Info]\033[0m Webmin Install Successfully !"
 echo ""
-echo " $IP:10000"
-echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "=============================="
+echo "  Succesfully Install Webmin  "
+echo "=============================="
+echo "http://$IP:10000"
+echo "======================="
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 m-webmin
 }
 function restart () {
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m        • RESTART WEBMIN •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo " Start Uninstall Webmin"
+clear
+echo ""
+echo "============================="
+echo " Succesfully Restart Webmin  "
+echo "============================="
 sleep 0.5
 echo ""
 echo " Restarting Webmin"
@@ -62,16 +63,17 @@ echo ""
 sleep 0.5
 echo -e "\033[32m[Info]\033[0m Webmin Start Successfully !"
 echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "============================="
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 m-webmin
 }
 function uninstall () {
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m       • UNINSTALL WEBMIN •        \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo "================================"
+echo "  Succesfully Uninstall Webmin  "
+echo "================================"
 sleep 0.5
 echo ""
 echo -e "\033[32m[Info]\033[0m Removing Repositori Webmin"
@@ -83,7 +85,7 @@ apt autoremove --purge webmin -y > /dev/null 2>&1
 sleep 0.5
 echo -e "\033[32m[Info]\033[0m Webmin Uninstall Successfully !"
 echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "================================"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 m-webmin
@@ -94,35 +96,22 @@ else
 sts="${Error}"
 fi
 clear 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m          • WEBMIN MENU •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-echo -e " Status $sts"
-echo -e " [\e[36m•1\e[0m] Install Webmin"
-echo -e " [\e[36m•2\e[0m] Restart Webmin"
-echo -e " [\e[36m•3\e[0m] Uninstall Webmin"
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo -e ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -rp " Please Enter The Correct Number : " -e num
+echo -e "================================"
+echo -e "      Webmin Menu $sts        "
+echo -e "================================"
+echo -e "   [•1] Install Webmin"
+echo -e "   [•2] Restart Webmin"
+echo -e "   [•3] Uninstall Webmin"
+echo -e "================================"
+read -rp "Please Enter The Correct Number : " -e num
 if [[ "$num" = "1" ]]; then
 install
 elif [[ "$num" = "2" ]]; then
 restart
 elif [[ "$num" = "3" ]]; then
 uninstall
-elif [[ "$num" = "0" ]]; then
-menu
-elif [[ "$num" = "x" ]]; then
-exit
 else
 clear
-echo " You Entered The Wrong Number"
-sleep 2
+echo " Boh salah tekan"
 m-webmin
 fi
