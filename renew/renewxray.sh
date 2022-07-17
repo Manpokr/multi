6acc277d2a69898b755bb21f1e005f4c6a136006
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-17 22:57:52 +0800

    Update renewxray.sh

--- renew/renewxray.sh
+++ renew/renewxray.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# Renew Trojan
+# // Renew Xray
 # ===============
 
-# Color
+# // Color
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
+LIGHT='\e[37m'
 
 # ==========================================
-# Getting
+# // Getting
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
@@ -17,7 +18,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 # Validate Your IP Address
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan/akun.conf")
+NUMBER_OF_CLIENTS=$(grep -c -E "^" "/etc/xray/clients.txt")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
@@ -27,32 +28,37 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan/akun.conf")
 
 	clear
 	echo ""
-	echo "Select the existing client you want to renew"
+	echo " Client xray renew"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
-	grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^" "/etc/xray/clients.txt" | cut -d ' ' -f 1-2 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
+                echo -e "==============================="
+			read -rp " Select one client [1]: " CLIENT_NUMBER
 		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+                echo -e "==============================="
+			read -rp " Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
 read -p "Expired (days): " masaaktif
-user=$(grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^" "/etc/xray/clients.txt" | cut -d ' ' -f 1 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^" "/etc/xray/clients.txt" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/trojan/akun.conf
+#sed -i "s/### $user $exp/### $user $exp4/g" /etc/trojan/akun.conf
+sed -i "/\b$user\b/d" /etc/xray/clients.txt
+echo -e "$user\t$exp3\t$uuid" >> /etc/xray/clients.txt
+
 clear
 echo ""
-echo " Trojan Account Was Successfully Renewed"
+echo " Client Xray Renewed"
 echo " =========================="
 echo " Client Name : $user"
 echo " Expired On  : $exp4"
 echo " =========================="
-echo "Script By Manternet"
+echo -e " Script By Manternet\e[37m "
