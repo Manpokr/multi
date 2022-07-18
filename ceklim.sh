207d766823882edca686bf5d7fd11cd0cc7dab9c
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-18 22:03:35 +0800

    Update ceklim.sh

--- ceklim.sh
+++ ceklim.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 RED='\033[0;31m'                                                                                          
-GREEN='\033[0;32m'                                                                                        
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'                                                                                         
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'                                                                                         
+GREEN='\033[0;32m'                                                                                                                                                                                 
 NC='\033[0;37m'
 LIGHT='\033[0;37m'
 
 # // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
 
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m   • Users Multi Login SSH •       \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "===========================================";
+echo -e "         • Users Multi Login SSH •       "
+echo -e "===========================================";
 echo " ";
 if [ -e "/root/log-limit.txt" ]; then
 echo "User Who Violate The Maximum Limit";
 echo "Time - Username - Number of Multilogin"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m";
+echo -e "===========================================";
 cat /root/log-limit.txt
 else
 echo " No user has committed a violation"
@@ -30,5 +25,6 @@ echo " "
 echo " The user-limit script not been executed."
 fi
 echo " ";
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m";
+echo -e "===========================================";
 echo " ";
+echo -e "Script By Manternet"
