b3e6ff6923399b01528a319570b3eb435d65394c
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-18 23:08:56 +0800

    Update certv2ray.sh

--- certv2ray.sh
+++ certv2ray.sh
@@ -9,32 +9,51 @@ NC='\033[0;37m'
 LIGHT='\033[0;37m'
 
 # // Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
 
+# // Getting
+MYIP=$(wget -qO- ifconfig.me/ip); 
+echo "Checking VPS" 
+IZIN=$(curl -sS https://raw.githubusercontent.com/Manpokr/mon/main/ip | awk '{print $4}' | grep $MYIP ) 
+if [[ $MYIP = $IZIN ]]; then 
+echo -e "${GREEN}Permission Accepted...${NC}" 
+else 
+echo -e "${RED}Permission Denied!${NC}"; 
+echo -e "${LIGHT}Please Contact Admin!!" 
+rm -f certv2ray
+exit 0
+fi
 clear
+
+# // Renew Cert
+sleep 0.5
 domain=$(cat /etc/xray/domain)
+
+# // ###
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 clear
-cd .acme.sh
-echo -e "${RED}starting....,${NC}" 
-echo "Port 80 Akan di Hentikan Saat Proses install Cert"    
 
-# // Update Sertificate SSL
-echo "Automatical Update Your Sertificate SSL"
-sleep 3
-echo Starting Update SSL Sertificate
-sleep 0.5
-source /var/lib/manpokr/ipvps.conf
-domain=$IP
+cd .acme.sh
+echo -e "\033[0;32mstarting........\033[m"
+echo -e "Port ${RED}80${NC} Akan di Hentikan Saat Proses install Cert"
+echo -e ""
+sleep 2
+
 systemctl stop nginx
+systemctl stop v2ray
+systemctl stop v2ray.service
 systemctl stop xray
 systemctl stop xray.service
 systemctl stop trojan
 systemctl stop trojan.service
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+
+bash acme.sh --set-default-ca --server letsencrypt
+bash acme.sh --issue -d $domain --standalone -k ec-256 --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+sleep 2
+
 systemctl daemon-reload
 systemctl restart nginx
 systemctl daemon-reload
