5d55a6693b2fa7604869e6680e46fcd21a019af3
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-20 01:27:59 +0800

    Update change-port.sh

--- change-port.sh
+++ change-port.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
 
 # // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
