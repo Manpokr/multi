ebd273b95b07d7da23de42e1bc9ca91adee06ab1
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-22 04:20:31 +0800

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -6,17 +6,6 @@ LIGHT='\033[0;37m'
 
 # // Getting
 domain=$(cat /etc/v2ray/domain)
-apt install iptables iptables-persistent -y
-apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
-apt install socat cron bash-completion ntpdate -y
-ntpdate pool.ntp.org
-apt -y install chrony
-timedatectl set-ntp true
-systemctl enable chronyd && systemctl restart chronyd
-systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Kuala_Lumpur
-chronyc sourcestats -v
-chronyc tracking -v
 date
 
 # // Add Folder
