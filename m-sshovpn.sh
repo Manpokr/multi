dfa0c39d8bc12928a793b2b11e3d8c8f05c21881
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-23 23:23:55 +0800

    Update m-sshovpn.sh

--- m-sshovpn.sh
+++ m-sshovpn.sh
@@ -35,7 +35,7 @@ echo -e ""
 read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
-1) clear ; usernew ; exit ;;
+1) clear ; addssh ; exit ;;
 2) clear ; trial ; exit ;;
 3) clear ; renew ; exit ;;
 4) clear ; hapus ; exit ;;
