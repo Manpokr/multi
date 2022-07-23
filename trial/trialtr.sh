926c66659280aa89606ad4b2dff472456c2d21b7
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-24 01:12:44 +0800

    Update and rename trialtrojan.sh to trialtr.sh

--- trial/trialtr.sh
+++ trial/trialtr.sh
@@ -1,45 +1,59 @@
 #!/bin/bash
-# Trojan
+# Trojan GFW
 # ==================
 
 # Color
-RED="\e[1;31m"
-GREEN="\e[0;32m"
-NC="\e[0m"
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
 
 # Validate Your IP Address
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 clear
+
 IP=$( curl -s ipinfo.io/ip );
 uuid=$(cat /etc/trojan/uuid.txt)
 source /var/lib/manpokr/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
 fi
-tr=$(cat /etc/trojan/config.json | grep local_port | sed 's/local_//g' | sed 's/port//g' | sed 's/://g' | sed 's/,//g' | sed 's/"//g' | sed 's/   //g' | sed 's/  //g')
-user=dev-`</dev/urandom tr -dc X-Z0-9 | head -c4`
+
+tr="$(cat ~/log-install.txt | grep -i "Trojan-GFW" | cut -d: -f2|sed 's/ //g')"
+user=TRIAL`</dev/urandom tr -dc c-z | head -c4`
 exp=1
 domain=$(cat /etc/v2ray/domain)
-read -p "Expired (days): " masaaktif
-read -p "SNI (BUG)     : " sni
+read -p "SNI (BUG) : " sni
 read -p "Subdomain (EXP : manternet.xyz. / Press Enter If Only Using Hosts) : " sub
 dom=$sub$domain
+
 sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan/akun.conf
 systemctl restart trojan
+
+# // Link
 trojanlink="trojan://${user}@${dom}:${tr}?sni=$sni#$user"
-echo -e "======TROJAN-======"
+clear
+echo -e "================================="
+echo -e "       TROJAN  GFW  TRIAL       "
+echo -e "================================="
 echo -e "Remarks   : ${user}"
 echo -e "IP/Host   : ${MYIP}"
 echo -e "Domain    : ${domain}"
 echo -e "Subdomain : $dom"
 echo -e "Port      : ${tr}"
 echo -e "Key       : ${user}"
+echo -e "================================="
+echo -e "Link TR  : ${trojanlink}"
+echo -e "================================="
 echo -e "Created   : $hariini"
 echo -e "Expired   : $exp"
-echo -e "=========================="
-echo -e "Link TR  : ${trojanlink}"
-echo -e "=========================="
+echo -e "================================="
+
 echo -e "Script By Manternet"
