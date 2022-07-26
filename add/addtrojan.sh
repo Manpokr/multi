d3ed94203bc22a8600580f0a34935722e851349a
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-26 16:40:36 +0800

    Update addtrojan.sh

--- add/addtrojan.sh
+++ add/addtrojan.sh
@@ -27,66 +27,52 @@ exit 0
 fi
 clear
 
-# // Add Xtls
-IP=$( curl -s ipinfo.io/ip );
-clear
-source /var/lib/manpokr/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat /etc/mon/xray/domain)
-else
-domain=$(cat /etc/mon/xray/domain)
-fi
+# // Add Trojan
+echo ""
+        read -p "Username  : " user
+	if grep -qw "$user" /etc/mon/xray/clients.txt; then
+		echo -e ""
+		echo -e "User ${RED}$user${NC} already exist !!!"
+		exit 1
+	fi
+    read -p "SNI (BUG) : " sni
+	read -p "Duration (day) : " duration
+	uuid=$(cat /proc/sys/kernel/random/uuid)
+	exp=$(date -d +${duration}days +%Y-%m-%d)
+	expired=$(date -d "${exp}" +"%d %b %Y")
+        hariini=$(date -d "0 days" +"%d-%b-%Y")
+	domain=$(cat /etc/mon/xray/domain)
+	xtls="$(cat ~/log-install.txt | grep -w "XRAY VLESS XTLS SPLICE" | cut -d: -f2|sed 's/ //g')"
+	email=${user}
 
-xtls="$(cat ~/log-install.txt | grep -w "XRAY VLESS XTLS SPLICE" | cut -d: -f2|sed 's/ //g')"
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "User: " -e user
-		CLIENT_EXISTS=$(grep -w $user /etc/mon/xray/clients.txt | wc -l)
-
-		if [[ ${CLIENT_EXISTS} == '1' ]]; then
-			echo ""
-			echo -e " Name was already use, please choose another name ${RED}!!!${NC}"
-			exit 1
-		fi
-	done
-read -p "Expired (days): " masaaktif
-read -p "SNI (bug) : " sni
-read -p "Subdomain (EXP : manternet.xyz. / Press Enter If Only Using Hosts) : " sub
-dom=$sub$domain
-exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-hariini=`date -d "0 days" +"%Y-%m-%d"`
-email=${user}
-uuid=$(cat /proc/sys/kernel/random/uuid)
-
-echo -e "${user}\t${uuid}\t${exp}" >> /etc/mon/xray/clients.txt
+        echo -e "${user}\t${uuid}\t${exp}" >> /etc/mon/xray/clients.txt
 
     cat /etc/mon/xray/conf/04_trojan_TCP_inbounds.json | jq '.inbounds[0].settings.clients += [{"password": "'${uuid}'","email": "'${email}'"}]' > /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json
 	mv -f /etc/mon/xray/conf/04_trojan_TCP_inbounds_tmp.json /etc/mon/xray/conf/04_trojan_TCP_inbounds.json
-
-# // Link Xtls
-IP=$( curl -s ipinfo.io/ip )
+       
 cat <<EOF >>"/etc/mon/config-user/${user}"
-trojan://$uuid@$dom:$xtls?sni=$sni#$user@manVPN
+trojan://$uuid@$domain:$xtls?sni=$sni#$user
 
 EOF
-
-# // CONF
-tr0="trojan://$uuid@$dom:$xtls?sni=$sni#$user"
-systemctl restart xray.service
-clear
-echo -e "================================="
-echo -e "       XRAY TROJAN CONFIG        "
-echo -e "================================="
-echo -e "Remarks        : ${user}"
-echo -e "IP/Host        : ${IP}"
-echo -e "Domain         : ${domain}"
-echo -e "Subdomain      : ${dom}"
-echo -e "Sni            : ${sni}"
-echo -e "port           : $xtls"
-echo -e "id             : ${uuid}"
-echo -e "================================="
-echo -e "Trojan TCP     : trojan://$uuid@$dom:$xtls?sni=$sni#$user@manVPN"
-echo -e "================================="
-echo -e "Created        : $hariini"
-echo -e "Expired On     : $exp"
-echo -e "================================="
-echo -e "Script By Manternet"
+ 
+    systemctl restart xray.service
+    echo -e "${CYAN}[Info]${NC} xray Start Successfully !"
+    sleep 2
+    clear
+    echo -e "=================================" 
+    echo -e "   XRAY TROJAN USER INFORMATION " 
+    echo -e "=================================" 
+    echo -e "Username : $user" 
+    echo -e "IP/Host  : ${MYIP}" 
+    echo -e "Domain   : ${domain}" 
+    echo -e "Sni      : ${sni}"
+    echo -e "Port     : $xtls"
+    echo -e "id       : $uuid"
+    echo -e "================================="
+    echo -e "Trojan TLS : trojan://$uuid@$domain:$xtls?sni=$sni#$user"
+    echo -e "================================="
+    echo -e "Created      : $hariini"
+    echo -e "Expired date : $expired"
+    echo -e "================================="
+    echo -e " ScriptMod By Manternet "
+    echo ""
