a02a5e789198c9eadace636ef81e3899bf43c89e
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-26 16:34:37 +0800

    Add files via upload

--- add/addvless.sh
+++ add/addvless.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 RED='\033[0;31m' 
 NC='\033[0m' 
 GREEN='\033[0;32m' 
@@ -46,30 +46,32 @@ echo ""
 	email=${user}
 
         echo -e "${user}\t${uuid}\t${exp}" >> /etc/mon/xray/clients.txt
-        cat /etc/rare/xray/conf/03_VLESS_WS_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","email": "'${email}'"}]' > /etc/rare/xray/conf/03_VLESS_WS_inbounds_tmp.json
-	mv -f /etc/rare/xray/conf/03_VLESS_WS_inbounds_tmp.json /etc/rare/xray/conf/03_VLESS_WS_inbounds.json
+
+        cat /etc/mon/xray/conf/03_VLESS_WS_inbounds.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","email": "'${email}'"}]' > /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json
+	mv -f /etc/mon/xray/conf/03_VLESS_WS_inbounds_tmp.json /etc/mon/xray/conf/03_VLESS_WS_inbounds.json
        
-cat <<EOF >>"/etc/rare/config-user/${user}"
-vless://$uuid@$domain:$xtls?encryption=none&security=xtls&sni=$BUG&type=ws&host=$BUG&path=/xrayws#$user@IanVPN
+cat <<EOF >>"/etc/mon/config-user/${user}"
+vless://$uuid@$domain:$xtls?encryption=none&security=tls&sni=$sni&type=ws&host=$sni&path=/xrayws#$user
 EOF
  
     systemctl restart xray.service
     echo -e "${CYAN}[Info]${NC} xray Start Successfully !"
     sleep 2
+    clear
     echo -e "=================================" 
     echo -e "   XRAY VLESS USER INFORMATION " 
     echo -e "=================================" 
-    echo -e " Username : $user" 
-    echo -e " IP/Host  : ${MYIP}" 
-    echo -e " Domain   : ${domain}" 
-    echo -e " Sni      : ${sni}"
-    echo -e " Port     : $xtls"
-    echo -e " id       : $uuid"
+    echo -e "Username : $user" 
+    echo -e "IP/Host  : ${MYIP}" 
+    echo -e "Domain   : ${domain}" 
+    echo -e "Sni      : ${sni}"
+    echo -e "Port     : $xtls"
+    echo -e "id       : $uuid"
     echo -e "================================="
-    echo -e " Vless TLS : vless://$uuid@$BUG.$domain:$xtls?encryption=none&security=xtls&sni=$BUG&type=ws&host=$BUG&path=/xrayws#$user@IanVPN"
+    echo -e "Vless TLS : vless://$uuid@$domain:$xtls?encryption=none&security=tls&sni=$sni&type=ws&host=$sni&path=/xrayws#$user"
     echo -e "================================="
-    echo -e " Created      : $hariini"
-    echo -e " Expired date : $expired"
+    echo -e "Created      : $hariini"
+    echo -e "Expired date : $expired"
     echo -e "================================="
     echo -e " ScriptMod By Manternet "
     echo ""
