ab1082ace7088f6b21b539db34188a3c4b25ac5b
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-28 19:19:05 +0800

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -50,13 +50,23 @@ END
 chmod 644 /root/.profile
 
 # // Nginx
-sudo apt install gnupg2 ca-certificates lsb-release -y 
-echo "deb http://nginx.org/packages/mainline/debian $(lsb_release -cs) nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
-echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx
-curl -o /tmp/nginx_signing.key https://nginx.org/keys/nginx_signing.key
+		sudo apt install gnupg2 ca-certificates lsb-release -y 
+		echo "deb http://nginx.org/packages/mainline/ubuntu $(lsb_release -cs) nginx" | sudo tee /etc/apt/sources.list.d/nginx.list 
+		echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx 
+		curl -o /tmp/nginx_signing.key https://nginx.org/keys/nginx_signing.key 
+		# gpg --dry-run --quiet --import --import-options import-show /tmp/nginx_signing.key
+		sudo mv /tmp/nginx_signing.key /etc/apt/trusted.gpg.d/nginx_signing.asc
+		sudo apt update 
+
+
+
+#sudo apt install gnupg2 ca-certificates lsb-release -y 
+#echo "deb http://nginx.org/packages/mainline/debian $(lsb_release -cs) nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+#echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx
+#curl -o /tmp/nginx_signing.key https://nginx.org/keys/nginx_signing.key
 # gpg --dry-run --quiet --import --import-options import-show /tmp/nginx_signing.key
-sudo mv /tmp/nginx_signing.key /etc/apt/trusted.gpg.d/nginx_signing.asc
-sudo apt update
+#sudo mv /tmp/nginx_signing.key /etc/apt/trusted.gpg.d/nginx_signing.asc
+#sudo apt update
 systemctl daemon-reload
 systemctl enable nginx
 
